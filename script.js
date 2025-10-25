fd92bed0347324cdc4691504dd4c14f3b8814fd7
Kumave <eli707jk@gmail.com>
2025-10-25 14:00:06 -0600

    Add contact button interaction effect

---
+++ script.js
@@ -0,0 +1,4 @@
+// Efecto de interacción con el botón de contacto
+document.getElementById("contact-btn").addEventListener("click", () => {
+  alert("🐾 Gracias por contactarnos 💖 Pronto nos comunicaremos contigo.");
+});
